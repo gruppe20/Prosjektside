e319b36e8e5ee77b8eed7eb72a22a64458cb7840
Ole Grinnby <ogrinnby@gmail.com>
2011-11-23 14:23:29 +0100

    Lagt til base for rails

---
+++ prosjektside/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prosjektside::Application.config.secret_token = 'd291ed0814be34c8899ca02d46049b4c9892d4f87079b659c93a63e2de659b2e2daf0653c98257da4e781e531ef9b7bd3f81e78d091f170eed2a6a02357ecc26'
