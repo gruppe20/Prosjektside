c231d1c634fac6a478f3a47e600c028ad61fecee
Jørgen Rieck <jurgemaister@gmail.com>
2012-01-12 12:09:49 +0100

    Added auth stuff

---
+++ prosjektside/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.authenticate(params[:username], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to admin_url, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid username or password"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
+end
---
+++ prosjektside/app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  attr_accessible :username, :password
+  
+  attr_accessor :password
+  before_save :encrypt_password
+  
+  validates_confirmation_of :password
+  validates_presence_of :username
+  
+  def self.validate(usename, password)
+    user = find_by_username(username)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+  
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+end
