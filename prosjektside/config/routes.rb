7418bf89a1e60da3c989a5edbd84e11a5024293b
Jørgen Rieck <jurgemaister@gmail.com>
2012-01-12 12:08:30 +0100

    Added auth stuff

--- prosjektside/config/routes.rb
+++ prosjektside/config/routes.rb
@@ -1,13 +1,12 @@
 Prosjektside::Application.routes.draw do
+  root :to => 'home#index'
+  
   get "protocol/index"
-
   get "home/index"
   get "admin/index"
   resources :home
   resources :members
-  resources :admin 
-  
-  root :to => 'home#index'
+  resources :admin
   
   match '/documents' => 'home#documents', :as => :documents
   match '/about_admin' => 'admin#about'
@@ -15,64 +14,7 @@ Prosjektside::Application.routes.draw do
   match '/protocol' => 'protocol#index', :as => :protocol
   match '/protocol/:url' => 'protocol#index', :constraints => {:url => /.*/}
   
-  
-  #match '/adm' => 'members#adm'
-  #match '/adm', :controller => 'members', :action => 'adm', :as => :adm
-  
-  #match '/members' => 'members#index'
-  #match '/members/:id' => 'members#new'
-  
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  resources :sessions
 end
